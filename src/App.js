e821adde7cc56c5cdf9f9e493bc7af65f71d1666
VishrutMargam <vishrutm@axioned.com>
2022-02-15 15:49:44 +0530

    feat: added rest of the components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import ProductListing from './components/ProductListing';
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <ProductListing />
     </div>
   );
 }
