7554157fa4fc5343df8cc41f5420619d87aae08b
VishrutMargam <vishrutm@axioned.com>
2022-02-15 15:58:21 +0530

    feat: added Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import ProductDetail from './components/ProductDetail';
 import ProductListing from './components/ProductListing';
 
 function App() {
 
   return (
-    <div className="App">
-      <Header />
-      <ProductListing />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
